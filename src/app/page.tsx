368c9b246ec4e53c89bbaa2ef9c9c286df43a9c9
isayme <isaymeorg@gmail.com>
2024-02-26 20:13:25 +0800

    处理手机100vh问题

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+'use client'
+
 import { FileUpload } from '@/components/file-upload'
 import { Toaster } from '@/components/ui/toaster'
+import { useEffect } from 'react'
 
 export default function Home() {
+  useEffect(function () {
+    let { innerHeight } = window
+    document.documentElement.style.setProperty('--100vh', `${innerHeight}px`)
+
+    function setRealHeight() {
+      let { innerHeight } = window
+      document.documentElement.style.setProperty('--100vh', `${innerHeight}px`)
+    }
+
+    window.addEventListener('resize', setRealHeight)
+    return () => window.removeEventListener('resize', setRealHeight)
+  }, [])
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-center'>
+    <main className='flex real100vh flex-col items-center justify-center'>
       <FileUpload></FileUpload>
       <Toaster />
     </main>
